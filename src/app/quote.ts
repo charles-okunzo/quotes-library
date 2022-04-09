17daaaeaac4e3a5c0e7916e102be4b0f06731f9f
charles okunzo <okunzopeter01@gmail.com>
2022-04-09 14:10:04 +0300

    style:published quoted-add quotes component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
 
-  constructor(public id:number, public quote:string, public author:string, public upvote:number, public downvote:number){
+  constructor(public id:number, public quote:string, public author:string, public upvote:number, public downvote:number, public publisher:string){
 
   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,8 +8,10 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
 
-  publishedQuote:Quote[]=[
-    new Quote(1, "A life not lived for others is not a life", "Mother Teresa", 0, 0)
+  pubdQuotes:Quote[]=[
+    new Quote(1, "A life not lived for others is not a life", "Mother Teresa", 0, 0, "Carol Robi"),
+    new Quote(2, "Glory lies in the attempt to reach one's goal and not in reaching it", "Mahatma Gandhi", 0, 0, "Walter Onzere"),
+    new Quote(3, "If you are walking down the right path and you're willing to keep walking, eventually you'll make progress", "Barrack Obama", 0, 0, "Angel Becky")
   ]
 
   constructor() { }
