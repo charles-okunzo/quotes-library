90f9441fcdce0a29918e10daab25e908726ab726
charles okunzo <okunzopeter01@gmail.com>
2022-04-11 08:51:18 +0300

    fix:modifies time since pipe to consider singular values

--- src/app/time-elapsed.pipe.ts
+++ src/app/time-elapsed.pipe.ts
@@ -11,28 +11,52 @@ export class TimeElapsedPipe implements PipeTransform {
     let currentTime:any= new Date();
     let secondsPassed= Math.floor((currentTime-value)/1000);
     let interval= secondsPassed/31536000;
-    if(interval>1){
+    if(interval>2){
       return `${Math.floor(interval)} years`
+    }
+    interval= secondsPassed/31536000;
+    if(interval>1){
+      return `${Math.floor(interval)} year`
+    }
+
+    interval= secondsPassed/2592000;
+    if(interval>2){
+      return `${Math.floor(interval)} months`
     }
 
     interval= secondsPassed/2592000;
     if(interval>1){
-      return `${Math.floor(interval)} months`
+      return `${Math.floor(interval)} month`
+    }
+
+    interval=secondsPassed/86400;
+    if(interval>2){
+      return `${Math.floor(interval)} days`
     }
 
     interval=secondsPassed/86400;
     if(interval>1){
-      return `${Math.floor(interval)} days`
+      return `${Math.floor(interval)} day`
+    }
+
+    interval=secondsPassed/3600;
+    if(interval>2){
+      return `${Math.floor(interval)} hours`
     }
 
     interval=secondsPassed/3600;
     if(interval>1){
-      return `${Math.floor(interval)} hours`
+      return `${Math.floor(interval)} hour`
+    }
+
+    interval=secondsPassed/60;
+    if(interval>2){
+      return `${Math.floor(interval)} minutes`
     }
 
     interval=secondsPassed/60;
     if(interval>1){
-      return `${Math.floor(interval)} minutes`
+      return `${Math.floor(interval)} minute`
     }
 
     return `${Math.floor(interval)} seconds`
