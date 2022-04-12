e1e2b4bdc158c5e86a0a224d8c010d932d520874
charles okunzo <okunzopeter01@gmail.com>
2022-04-12 13:03:41 +0300

    fix:modify pipe to display recent time as just now

--- src/app/time-elapsed.pipe.ts
+++ src/app/time-elapsed.pipe.ts
@@ -12,51 +12,51 @@ export class TimeElapsedPipe implements PipeTransform {
     let secondsPassed= Math.floor((currentTime-value)/1000);
     let interval= secondsPassed/31536000;
     if(interval>2){
-      return `${Math.floor(interval)} years ago`
+      return `${Math.floor(interval)} years`
     }
     interval= secondsPassed/31536000;
     if(interval>1){
-      return `${Math.floor(interval)} year ago`
+      return `${Math.floor(interval)} year`
     }
 
     interval= secondsPassed/2592000;
     if(interval>2){
-      return `${Math.floor(interval)} months ago`
+      return `${Math.floor(interval)} months`
     }
 
     interval= secondsPassed/2592000;
     if(interval>1){
-      return `${Math.floor(interval)} month ago`
+      return `${Math.floor(interval)} month`
     }
 
     interval=secondsPassed/86400;
     if(interval>2){
-      return `${Math.floor(interval)} days ago`
+      return `${Math.floor(interval)} days`
     }
 
     interval=secondsPassed/86400;
     if(interval>1){
-      return `${Math.floor(interval)} day ago`
+      return `${Math.floor(interval)} day`
     }
 
     interval=secondsPassed/3600;
     if(interval>2){
-      return `${Math.floor(interval)} hours ago`
+      return `${Math.floor(interval)} hours`
     }
 
     interval=secondsPassed/3600;
     if(interval>1){
-      return `${Math.floor(interval)} hour ago`
+      return `${Math.floor(interval)} hour`
     }
 
     interval=secondsPassed/60;
     if(interval>2){
-      return `${Math.floor(interval)} minutes ago`
+      return `${Math.floor(interval)} minutes`
     }
 
     interval=secondsPassed/60;
     if(interval>1){
-      return `${Math.floor(interval)} minute ago`
+      return `${Math.floor(interval)} minute`
     }
 
     return `${Math.floor(interval)} seconds`
